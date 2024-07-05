029b8f9d67f1e661a89a7e06c3f49db75dfde856
sharma-dikshant <dikshantsharma2005@gmail.com>
2024-07-05 14:12:58 +0530

    completed add friend function

--- src/App.js
+++ src/App.js
@@ -83,12 +83,42 @@ function Friend({ friend }) {
 }
 
 function FormAddFriend() {
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("https://i.pravatar.cc/48");
+
+  function handleOnSubmit(evt) {
+    evt.preventDefault();
+
+    if (!name || !image) return;
+
+    const id = crypto.randomUUID();
+    const newFriend = {
+      id,
+      name,
+      image: `${image}?=${id}`,
+      balance: 0,
+    };
+
+    console.log(newFriend);
+
+    setName("");
+    setImage("https://i.pravatar.cc/48");
+  }
+
   return (
-    <form className="form-add-friend">
+    <form className="form-add-friend" onSubmit={handleOnSubmit}>
       <label>👯‍♀️Friend name</label>
-      <input type="text" />
+      <input
+        type="text"
+        value={name}
+        onChange={(evt) => setName(evt.target.value)}
+      />
       <label>👤Image URL</label>
-      <input type="text" />
+      <input
+        type="text"
+        value={image}
+        onChange={(evt) => setImage(evt.target.value)}
+      />
       <Button>Add</Button>
     </form>
   );
